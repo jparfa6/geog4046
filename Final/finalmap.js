4bad2b1b9837b80d1503adea058741aa350fcabe
jparfa6 <48488062+jparfa6@users.noreply.github.com>
2019-05-02 00:42:26 -0500

    Add files via upload

---
+++ Final/finalmap.js
@@ -0,0 +1,21 @@
+let finalmap = L.map('finalmap').setView([29.37, -90.31], 4)
+
+let thisBasemapURL = ('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
+let thisBasemap = L.tileLayer(thisBasemapUrl)
+thisBasemap.addTo(finalmap)
+
+let thisBasemap2Url = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+let thisBasemap2 = L.tileLayer(thisBasemap2Url)
+
+let thisBasemap3Url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+let thisBasemap3 = L.tileLayer(thisBasemap3Url)
+
+let thisBasemap4Url = 'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}'
+let thisBasemap4 = L.tileLayer(thisBasemap4Url)
+
+let theseBasemaps = {
+  'Basic Map': thisBasemap,
+  'Topo Map': thisBasemap2,
+  'World Imagery Map': thisBasemap3,
+  'Equator Map': thisBasemap4
+}
